4eb9d376f9eb0ea17367d380f773b1594947d0ad
Viktoria <viktoria.vaseva@gmail.com>
2019-04-20 18:50:28 +0300

    Работа с датами

--- function.php
+++ function.php
@@ -10,6 +10,23 @@ function table_tasks($list_tasks, $category_projects)
     return $number;
 };
 
+date_default_timezone_set('Europe/Moscow');
+setlocale(LC_ALL, 'ru_RU');
 
+/*function rest_hours($day) {
+    $fix_date = date_create ($day);
+    $curdate = date_create("now");
+    $diff = date_diff($curdate, $fix_date);
+    $hour_count = date_interval_format($diff,"%H");
 
+    return $hour_count;
+}*/
 
+function rest_hours($day) {
+    $secs_in_hour = 3600;
+    $fix_date = strtotime($day);
+    $curdate = time();
+    $count_diff = $curdate - $fix_date;
+    $count_hour = $count_diff / $secs_in_hour;
+     return $count_hour;
+    }
