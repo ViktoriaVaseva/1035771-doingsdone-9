7235e8c7b19d2105b3a763823aeb55fa6f45a747
Viktoria <viktoria.vaseva@gmail.com>
2019-04-21 23:38:53 +0300

    Создание схемы БД личного проекта

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE daily_plan;
 
+USE daily_plan;
 CREATE TABLE user (
-id       INT AUTO_INCREMENT PRIMARY KEY,
-name     char(64) NOT NULL UNIQUE,
-email    CHAR(128) NOT NULL UNIQUE,
-password CHAR(64) UNIQUE,
-date     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+id                 INT AUTO_INCREMENT PRIMARY KEY,
+user_name          CHAR(64) NOT NULL UNIQUE,
+email              CHAR(128) NOT NULL UNIQUE,
+password           CHAR(64) UNIQUE,
+registration_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE project (
@@ -16,11 +16,11 @@ user_id  INT
 
 CREATE TABLE task (
 id         INT AUTO_INCREMENT PRIMARY KEY,
-name       CHAR(64),
+title      CHAR(64),
 dt_add     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 status     INT,
-fail       CHAR(256),
-valid      TIMESTAMP,
+url_file   CHAR(128),
+deadline   TIMESTAMP,
 user_id    INT,
 project_id INT
 );
